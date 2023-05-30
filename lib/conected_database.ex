a99dbf088057c0b5ccabdecf72545035aa7e4655
Gabriel Lucas Rocca Esquilage <gabriel.esquilage@hotsoft.com.br>
2023-05-30 09:51:13 -0300

    primeiroCommit

---
+++ lib/conected_database.ex
@@ -0,0 +1,18 @@
+defmodule ConectedDatabase do
+  @moduledoc """
+  Documentation for `ConectedDatabase`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConectedDatabase.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/conected_database_test.exs
@@ -0,0 +1,8 @@
+defmodule ConectedDatabaseTest do
+  use ExUnit.Case
+  doctest ConectedDatabase
+
+  test "greets the world" do
+    assert ConectedDatabase.hello() == :world
+  end
+end
